f8cfaa4879829da0fb0b9d0cbdab062811ccf5e0
ybdesire <ybdesire@gmail.com>
2015-07-12 20:15:05 +0800

    Extract code of userRegister into module 'api_user_register.py' for long program better management.

---
+++ src/api/SyncFileAPI/v1/api_user_register.py
@@ -0,0 +1,40 @@
+from django.shortcuts import render
+from django.http import HttpResponse
+import json
+from django.contrib.auth.hashers import make_password, check_password, is_password_usable
+from .models import UserInfo
+
+def isStrEmpty(str):
+	if str and str.strip():# if string empty or blank
+		return False
+	else:
+		return True
+
+def createJsonResponseForUserRegister(register_status, error_code, msg):#error code = 100x
+	json_response = {}
+	json_response['register_status']=register_status		
+	json_response['error_code']=error_code
+	json_response['msg']=msg
+	return json_response
+
+def API_UserRegister(request):
+	req_user_name = request.GET.get('username', '')
+	req_password = request.GET.get('password', '')
+	req_email = request.GET.get('email', '')
+	
+	response_data = {}
+	
+	if not (isStrEmpty(req_user_name) or isStrEmpty(req_password) or isStrEmpty(req_email)):
+		if not ( UserInfo.objects.filter(userName = req_user_name) or UserInfo.objects.filter(email=req_email)):
+			response_data = createJsonResponseForUserRegister('ok', 1000, 'registered a new user.')
+			print('passwd: {0}'.format(make_password(req_password)))
+			q = UserInfo(userName=req_user_name, password=make_password(req_password), email=req_email)
+			q.save()
+		elif UserInfo.objects.filter(userName = req_user_name):
+			response_data = createJsonResponseForUserRegister('error', 1001, 'the user has been registered, please change the user name.')
+		elif UserInfo.objects.filter(email=req_email):
+			response_data = createJsonResponseForUserRegister('error', 1002, 'the email has been registered, please change the email.')
+	else:
+		response_data = createJsonResponseForUserRegister('error', 1003, 'blank username/password/email')
+	
+	return response_data
