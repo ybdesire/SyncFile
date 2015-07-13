3bd5018a466abaa5b7dc5e6be2b8b1d605fffe12
ybdesire <ybdesire@gmail.com>
2015-07-13 21:37:29 +0800

    Add v1 API UserRegister auto test cases.

--- src/api/SyncFileAPI/v1/tests.py
+++ src/api/SyncFileAPI/v1/tests.py
@@ -1,3 +1,26 @@
 from django.test import TestCase
+import unittest
+import requests
+import json
 
 # Create your tests here.
+class TestAPIv1(unittest.TestCase):
+	def test_API_UserRegister_1000(self):
+		req = requests.get('http://localhost:8000/v1/userRegister?op=register&fmt=json&username=userxxx&password=password&email=userxxx@email.com')
+		answer = req.json()
+		self.assertEqual(answer['error_code'], 1000)
+
+	def test_API_UserRegister_1001(self):
+		req = requests.get('http://localhost:8000/v1/userRegister?op=register&fmt=json&username=user&password=password&email=user@email.com')
+		answer = req.json()
+		self.assertEqual(answer['error_code'], 1001)
+
+	def test_API_UserRegister_1002(self):
+		req = requests.get('http://localhost:8000/v1/userRegister?op=register&fmt=json&username=userx&password=password&email=user@email.com')
+		answer = req.json()
+		self.assertEqual(answer['error_code'], 1002)
+
+	def test_API_UserRegister_1003(self):
+		req = requests.get('http://localhost:8000/v1/userRegister?op=register&fmt=json&username= &password=password&email=user@email.com')
+		answer = req.json()
+		self.assertEqual(answer['error_code'], 1003)
