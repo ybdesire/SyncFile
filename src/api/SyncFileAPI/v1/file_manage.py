705371cb70df91279af3a8feb5e5886c9af4f5dc
ybdesire <ybdesire@gmail.com>
2015-07-25 17:28:06 +0800

    Basic logic for folder/file operation middle layer.

--- src/api/SyncFileAPI/v1/file_manage.py
+++ src/api/SyncFileAPI/v1/file_manage.py
@@ -1,22 +1,48 @@
 import os
+import shutil
 
 class fileManage:
+	'''parameters
+		basedir: 	absolute path for data storage. such as 'c:\\userdata'
+		path:		relative path. such as 'user1\\ooo'
+	'''
 	def __init__(self, basedir=''):
-		self.base_dir = os.getcwd() + '\\userdata\\'
+		self.base_dir =  '{0}\\userdata\\'.format(os.getcwd())
 	
+	'''create folder at relative path, the uplevel folder should exist'''
 	def create_folder(self, path):
 		try:
-			os.mkdir(self.base_dir + path)
+			os.mkdir('{0}{1}'.format(self.base_dir, path))
+			return True,''
 		except Exception as e:
-			print(e)
+			return False, e
 	
-	def fun(self):
-		print('xxx: {0}'.format(self.base_dir))
+	'''delete all files and folders under path, include the dir at path level'''
+	def delete_folder(self, path):
+		try:
+			full_path = '{0}{1}'.format(self.base_dir, path)
+			shutil.rmtree(full_path)
+			return True, ''
+		except Exception as e:
+			return False, e
+	
+	'''rename a folder's name'''
+	def rename_folder(self, path, name):
+		try:
+			full_path_src = '{0}{1}'.format(self.base_dir, path)
+			full_path_dst = '{0}{1}'.format(self.base_dir, os.path.split(path)[0]+'\\'+name)
+			os.rename(full_path_src, full_path_dst)
+			return True, ''
+		except Exception as e:
+			return False, e		
 		
 def main():
 	mgr = fileManage()
-	mgr.create_folder(r'user1')
-	mgr.fun()
-		
+	stat = mgr.delete_folder(r'user1\kkk')
+	if stat[0]:
+		print('success')
+	else:
+		print(stat[1])
+
 if __name__=='__main__':
 	main()
