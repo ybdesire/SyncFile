3097ca30d12e9c2e6373b8a6955034826a6f3d6b
ybdesire <ybdesire@gmail.com>
2015-08-27 21:35:40 +0800

    Distinguish the POST request from pure post(ajax/requests) or from http form element. Their posted data structure is different.

--- src/api/SyncFileAPI/v1/api_file.py
+++ src/api/SyncFileAPI/v1/api_file.py
@@ -1,3 +1,4 @@
+
 def create_basic_json_response(error_code, msg, status):
 	json_response = {}
 	json_response['error_code'] = error_code
@@ -9,9 +10,22 @@ def api_file(request):
 	'''authid should be validated before this function'''
 	response_data = 'file op'
 	req_op = request.GET.get('op', '')
+
 	if(request.method == 'POST'):
 		if(req_op == 'upload'):
-			response_data = 'upload'
+			print(request.body)
+			content_type_httpheader = request.META.get('CONTENT_TYPE')#get http header parameter from client
+			print(content_type_httpheader)
+			if(content_type_httpheader != 'text/plain'):#check if post request from html form element
+				print(content_type_httpheader)
+				content_type_type = content_type_httpheader.split(';')[0]
+				content_type_boundary = content_type_httpheader.split('=')[1]
+				print('content_type_type: {0}'.format(content_type_type))
+				print('content_type_boundary: {0}'.format(content_type_boundary))
+				if(content_type_type == 'multipart/form-data'):#if post request from html form element, the data structure is different from pure post
+					response_data = 'post from form'
+			else:#if the post request from pure post, the request.body is file content
+				response_data = 'post from pure post'
 		else:
 			response_data = create_basic_json_response(1201, 'not support this op', 'error')
 		
